f29b3f69935be8e7a8a59afbc778cd3fa7d2b8c5
squirrel-of-doom <squirrel.van.doom@gmail.com>
2014-10-24 13:03:32 +0200

    Made Pythonic

--- make_bitcount_header.py
+++ make_bitcount_header.py
@@ -13,23 +13,15 @@ N_BITS = 9
 # Array of powers of two
 POWERS_OF_TWO = [2**n for n in reversed(range(N_BITS))]
 
-# Function count_bits
-# IN: bitmask -- OUT: Number of bits set
-def count_bits(bitmask):
-    count = 0
-    dummy = bitmask
-    for power in POWERS_OF_TWO:
-        if dummy >= power:
-            count += 1
-            dummy -= power
-    return count
 
 def make_bitcount_dict():
-    return {bitmask: count_bits(bitmask) for bitmask in range(2**N_BITS)}
-    
+    return {bitmask: bin(bitmask).count('1') for bitmask in range(2**N_BITS)}
+
+
 def make_bitcount_list():
-    return [count_bits(bitmask) for bitmask in range(2**N_BITS)]
-    
+    return [bin(bitmask).count('1') for bitmask in range(2**N_BITS)]
+
+
 def make_header_file():
     filelines = []
     filelines.append("/*****************************************\n")
@@ -41,12 +33,13 @@ def make_header_file():
     filelines.append(" * for the bitmasks represented by 0..511\n")
     filelines.append(" ****************************************/\n")
 
-    filelines.append("const int BITCOUNT[] = {%s};\n\n" % repr(make_bitcount_list()).strip('[]'))
-    
+    list_str = repr(make_bitcount_list()).strip('[]')
+    filelines.append("const int BITCOUNT[] = {%s};\n\n" % list_str)
+
     with open('bitcount.h', 'w') as headerfile:
         headerfile.writelines(filelines)
-    
+
     return 0
-    
+
 if __name__ == '__main__':
     make_header_file()
